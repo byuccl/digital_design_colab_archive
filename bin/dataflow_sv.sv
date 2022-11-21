f1c604ee66ea50eb835ab07e915fbde419f5f014
Zach227 <zadriskill@gmail.com>
2022-11-21 14:40:56 -0700

    Fixed up Lab1

--- bin/dataflow_sv.sv
+++ bin/dataflow_sv.sv
@@ -5,20 +5,38 @@ module dataflow_sv(
 );
 
 always_comb begin
+    led = sw;
     if(btnl) begin
-//Function 1
-        end
+        led[15:8] = 8'h00;
+        led[7:0] = sw[15:8]^sw[7:0];
+    end
     else if(btnr) begin
-//Function 2
-        end
+        led[15:1] = 15'd0;
+        led[0] = (sw[0]&~sw[1])|(sw[2]);
+    end
     else if(btnd) begin
-//Function 3
+        led = sw << 3;
     end
     else if(btnu) begin
-//Function 4
+        if(sw[1] && !sw[0]) begin
+            if((sw[6:3] > 12) || (sw[6:3] == 0))
+                led = 16'h0000;
+            else 
+                led = 16'hFFFF;
+        end
+        else if(sw[0] && !sw[1]) begin
+            if((sw[6:3] >= 7) && (sw[6:3] <= 11) && (sw[2] == 0))
+                led = 16'hFFFF;
+            else if((sw[6:3] >= 1) && (sw[6:3] <= 10) && (sw[2] == 1))
+                led = 16'hFFFF;
+            else if((sw[6:3] == 12) && (sw[2] == 1))
+                led = 16'hFFFF;
+            else 
+                led = 16'h0000;
+        end
+        else
+            led = 16'h0000;
     end
-    else
-        led = sw;
 end
 
 endmodule
