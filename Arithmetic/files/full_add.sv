0cff5b02fe2b01d08fcaacc9e74720d6831348e1
westonMS <97260484+westonMS@users.noreply.github.com>
2023-01-11 19:40:25 -0700

    Update full_add.sv

--- Arithmetic/files/full_add.sv
+++ Arithmetic/files/full_add.sv
@@ -2,5 +2,6 @@ module full_add(
   input logic a, b, cin,
   output logic s, co
   );
-  //A, B and the cin should be added together. s is the result and co signifies carry over.
+  assign s = a^b^cin;
+  assign co = (a&b)|(a&cin)|(b&cin);
   endmodule
