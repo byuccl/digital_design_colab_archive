46ff271ada6c7f8d81adc41e95dec7b94da119dc
Zach227 <zadriskill@gmail.com>
2023-01-20 11:57:02 -0700

    updated solutions for stopwatch

--- Stopwatch/files/stopwatch.sv
+++ Stopwatch/files/stopwatch.sv
@@ -1,16 +1,18 @@
 module stopwatch(
-  //Signals
-  input wire logic clk, reset, run,
-  output logic [3:0] digit0, digit1, digit2, digit3
-);
-  //Code
-  logic rollover1, rollover2, rollover3, rollover4;
-  mod_counter #(32'd1000000) MILLISECOND (.clk(clk), .reset(reset), .increment(run), .rollover(rollover1));
-  mod_counter #(4'd10) HUNDREDTHS (.clk(clk), .reset(reset), .increment(rollover1), .rollover(rollover2), .count(digit0));
-  mod_counter #(4'd10) TENTHSS (.clk(clk), .reset(reset), .increment(rollover2), .rollover(rollover3), .count(digit1));
-  mod_counter #(4'd10) SECONDS (.clk(clk), .reset(reset), .increment(rollover3), .rollover(rollover4), .count(digit2));
-  mod_counter #(4'd6) TENS_SECONDS (.clk(clk), .reset(reset), .increment(rollover4), .count(digit3));
-
-
-
+    input wire logic clk, reset, run,
+    output logic [3:0] digit0, digit1, digit2, digit3
+    );
+    
+    //internal signals
+    logic [19:0] count;
+    logic [3:0] rollover;
+    logic pulse;
+    
+    //counter to roll over every .01 seconds and then for each digit
+    mod_counter #(1000000, 20) C4(.count(count), .rollover(pulse), .clk(clk), .reset(reset), .increment(run));
+    mod_counter C0(.count(digit0), .rollover(rollover[0]), .clk(clk), .reset(reset), .increment(pulse));
+    mod_counter C1(.count(digit1), .rollover(rollover[1]), .clk(clk), .reset(reset), .increment(rollover[0]));
+    mod_counter C2(.count(digit2), .rollover(rollover[2]), .clk(clk), .reset(reset), .increment(rollover[1]));
+    mod_counter #(6, 4) C3(.count(digit3), .rollover(rollover[3]), .clk(clk), .reset(reset), .increment(rollover[2]));
+    
 endmodule
