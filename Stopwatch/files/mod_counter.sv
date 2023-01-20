6e354e1def596c63470772d3b9142a5812ba45e4
Weston Smith <wms29@byu.edu>
2023-01-19 18:08:49 -0700

    Answers for stopwatch

--- Stopwatch/files/mod_counter.sv
+++ Stopwatch/files/mod_counter.sv
@@ -1,7 +1,26 @@
 module mod_counter#(
   //Add the parameter
+  parameter MOD_VALUE = 4'ha;
 )(
   //Add the Inputs exactly as listed
+  input wire logic clk, reset, increment,
+  output logic rollover,
+  output logic [3:0] count
 );
   //Code
+
+  always_ff@(posedge clk) begin
+    increment <= 0;
+    if(reset)
+      count <= 0;
+    else if(increment)
+      if(count == MOD_VALUE - 1)
+        begin
+        count <= 0;
+        end
+      else if (count == MOD_VALUE - 2)
+        increment <= 1;
+      else
+        count <= count + 1;
+  end
 endmodule
--- Stopwatch/files/stopwatch.sv
+++ Stopwatch/files/stopwatch.sv
@@ -1,5 +1,16 @@
 module stopwatch(
   //Signals
+  input wire logic clk, reset, run,
+  output logic [3:0] digit0, digit1, digit2, digit3
 );
   //Code
+  input wire logic rollover1, rollover2, rollover3, rollover4;
+  mod_counter #(8'd10000) MILLISECOND (.clk(clk), .reset(reset), .increment(run), .rollover(rollover1));
+  mod_counter #(4'd10) HUNDREDTHS (.clk(clk), .reset(reset), .increment(rollover1), .rollover(rollover2), .count(digit0));
+  mod_counter #(4'd10) TENTHSS (.clk(clk), .reset(reset), .increment(rollover2), .rollover(rollover3), .count(digit1));
+  mod_counter #(4'd10) SECONDS (.clk(clk), .reset(reset), .increment(rollover3), .rollover(rollover4), .count(digit2));
+  mod_counter #(4'd6) TENS_SECONDS (.clk(clk), .reset(reset), .increment(rollover4), .count(digit3));
+
+
+
 endmodule
