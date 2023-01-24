3ade9c295efe1986c8f562b8bee8eeafe1abb3cf
westonMS <97260484+westonMS@users.noreply.github.com>
2023-01-23 18:00:06 -0700

    Update stopwatch_top.sv

--- Stopwatch/files/stopwatch_top.sv
+++ Stopwatch/files/stopwatch_top.sv
@@ -8,7 +8,7 @@ module stopwatch_top(
     logic [15:0] data;
     
     SevenSegmentControl SS0(.clk(clk), .reset(btnc), .dataIn(data), .digitDisplay(4'b1111), .digitPoint(4'b0100), .anode(anode), .segment(segment));
-    stopwatch SW0(.clk(clk), .reset(btnc), .run(sw[0]), .digit0(data[3:0]), .digit1(data[7:4]), .digit2(data[11:8]), .digit3(data[15:12]));
+    stopwatch SW0(.clk(clk), .reset(btnc), .run(sw), .digit0(data[3:0]), .digit1(data[7:4]), .digit2(data[11:8]), .digit3(data[15:12]));
     
     
 endmodule
